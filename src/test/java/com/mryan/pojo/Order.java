51ed3393c586f8c0790f90e65102d5b40458bd44
ryan <770255975@qq.com>
2021-08-28 10:58:07 +0800

    feat(test 延迟加载)

--- src/test/java/com/mryan/pojo/Order.java
+++ src/test/java/com/mryan/pojo/Order.java
@@ -1,6 +1,8 @@
 package com.mryan.pojo;
 
-public class Order {
+import java.io.Serializable;
+
+public class Order implements Serializable {
 
     private Integer id;
     private String orderTime;
